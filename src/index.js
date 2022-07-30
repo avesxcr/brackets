43429584241061b1e662766e9587a306d9f6cdde
Sevastsyan <avesxcr@gmail.com>
2022-07-30 17:01:38 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length %2 != 0) {
+    return false;
+  }
+  
+  do  {
+    let length = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '')
+    }
+  
+    if (str.length === 0 || str.length === length) {
+      break;
+    }
+  } while (true)
+  return str.length === 0;
 }
+
